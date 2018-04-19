5335cebdb4a051d7a554f15515e18a38f808708d
goze <goze.qiu@gmail.com>
2018-04-19 16:35:33 +0800

    gulp task add clean & watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var del = require("del");
 var tslint = require("gulp-tslint");
 var runSequence = require('gulp-Sequence');
 var tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('clean', function (cb) {
+  return del(['lib'], cb);
+});
 
 gulp.task('tsc', function () {
   return gulp.src('./src/**/**.ts')
@@ -22,7 +27,13 @@ gulp.task('lint', function () {
 });
 
 gulp.task('build', function (cb) {
-  runSequence('lint', 'tsc')(cb);
-})
+  runSequence('lint','clean', 'tsc')(cb);
+});
+
+gulp.task('watch', function () {
+  gulp.watch(["src/**/*.ts"], ['build']).on('change', function (e) {
+      console.log('TypeScript file ' + e.path + ' has been changed. Compiling.');
+  });
+});
 
 gulp.task('default', ['build']);
